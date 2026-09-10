8259629dde210664e9ef7fe2d8e28da2277c3283
Tirtho-Ray <tirthoray10@gmail.com>
2026-09-10 23:38:13 +0600

    write variable in bash

--- hello.sh
+++ hello.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hello world"
+
+echo "This is a simple bash script that prints"
+
+echo "Hello world to the console"
+
+# This is a simple bash script that prints "Hello world" to the console. To run this script, you need to give it execute permissions using the command `chmod +x hello.sh`, and then you can execute it by running `./hello.sh`.
---
+++ variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+APP_NAME="MyApp"
+ENVIRONMENT="production"
+
+echo "Application Name: $APP_NAME"
+echo "Environment: $ENVIRONMENT"
+
